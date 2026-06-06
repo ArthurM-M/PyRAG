72ab181bc75816e4070079c47a5aaf6132d2cb34
ArthurM-M <arthurmmonteiro2906@gmail.com>
2026-06-06 15:42:18 -0300

    Implemented bm25

--- main.py
+++ main.py
@@ -1,17 +1,42 @@
 from google import genai
 from dotenv import load_dotenv
+from bm25 import BM25
 import os
 
 load_dotenv()
+
+documentos = [
+    "A política de reembolso da empresa garante devolução do dinheiro em até 7 dias úteis.",
+    "O horário de atendimento do suporte técnico é de segunda a sexta, das 8h às 18h.",
+    "Para resetar sua senha, clique em 'Esqueci minha senha' na tela de login e siga as instruções.",
+    "A garantia dos produtos eletrônicos é de 1 ano contra defeitos de fabricação."
+]
+
+searcher = BM25(documentos)
 
 client = genai.Client(api_key=os.getenv("API_KEY"))
 
 while True:
     question = input("Você: ")
 
+    contexto = searcher.buscar_melhor_contexto(question)
+
+    prompt_contexto = f"""
+    Você é um assistente virtual. Use o Contexto para responder à Pergunta.
+    Se o contexto não for útil, responda "Não tenho dados suficientes para responder corretamente.".
+
+    Contexto:
+    {contexto}
+
+    Pergunta:
+    {question}
+    """
+
+    print(f"Prompt com contexto: {prompt_contexto}")
+
     response = client.models.generate_content(
         model= "gemini-3.1-flash-lite",
-        contents= question
+        contents= prompt_contexto
     )
 
     print(f"Chat: {response.text}")
